01797049a46d7c912308a23a3bdbed7b8f08f5af
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-11-02 01:07:01 +0200

    As

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 let Z = document.querySelector('.z')
 let Y = document.querySelector('.y')
 let X = document.querySelector('.x')
+let plane = document.querySelector('.plane')
+console.log(plane)
 
 window.addEventListener("deviceorientation",(e)=>{
     console.log(e.alpha)
@@ -9,4 +11,7 @@ window.addEventListener("deviceorientation",(e)=>{
     Y.textContent = Math.round(e.gamma)
     console.log(e.beta)
     X.textContent = Math.round(e.beta)
+
+    console.log(`rotateZ(${Math.round(e.alpha)}deg) rotateY(${Math.round(e.gamma)}deg) rotateX(${Math.round(e.beta)}deg)`)
+    plane.style.transform = `rotateZ(${Math.round(e.alpha)}deg) rotateY(${Math.round(e.gamma)}deg) rotateX(${Math.round(e.beta)}deg)`;
 })
